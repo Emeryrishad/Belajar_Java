08b4e304a2f6f56278de13735141e0a65a222b8d
Ahmad Emery <112999609+Emeryrishad@users.noreply.github.com>
2022-09-07 04:43:59 +0000

    Belajar Variable 2

--- Variable.java
+++ Variable.java
@@ -1,12 +1,14 @@
-class Main {
+class Variable {
+  
   public static void main(String[] args) {
 
     String nama = " Ahmad Emery Rishad";
 
     int Angka_1 = 500;
 
-    System.out.println("Hello world!" + nama);
-    System.out.println("HeeHee");
+    System.out.println("Halo" + nama);
+    System.out.println("Uang"); 
+    System.out.println(3 + Angka_1);
     
 
   }
