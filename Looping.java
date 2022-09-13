7500e5bc85f1ca41d2b5f3dc193e766f82b0c1d4
Ahmad Emery <112999609+Emeryrishad@users.noreply.github.com>
2022-09-13 05:36:51 +0000

    Pertemuan Ke-2

--- Looping.java
+++ Looping.java
@@ -2,5 +2,13 @@ class Looping {
 
   public static void main(String[] args) {
     
+    System.out.println ("Looping dengan For Loop");
+    for (int i=10; i>0; i--){
+      System.out.println(i);
+    }
+
+    //System.out.println ("Looping dengan While")
+    //int a = 10;
+    //while (a < 12)
   }
 }
